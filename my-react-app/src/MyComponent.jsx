429a8af2779738efc328f941e50a7188ac7d8191
LJAMINE <aminelmgrmj@gmail.com>
2025-04-08 14:08:36 +0100

    onchange event handler

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,34 +1,56 @@
 import React, { useState } from "react";
 
+
+// function MyComponents() {
+//   const [name, setName] = useState("guest");
+//   const [age, setAge] = useState(0);
+
+//   const[isEmployed,setisEmployed]=useState(false);
+//   const updateName = () => {
+//     setName("sponge");
+//   };
+
+//   const updateAge = () => {
+//     setAge(age + 1);
+//   };
+
+//   const toggleStatus=()=>{
+//     setisEmployed(!isEmployed)
+//   }
+
+//   return (
+//     <div>
+//       <p>Name:{name}</p>
+//       <button onClick={updateName}>set Name</button>
+
+//       <p>age:{age}</p>
+//       <button onClick={updateAge}>set Name</button>
+
+//       <p>isEmployed:{isEmployed?"yes":"no"}</p>
+//       <button onClick={toggleStatus}>toogle status</button>
+//     </div>
+//   );
+// }
+
+// export default MyComponents;
+
+
+
+// onchange-----------------------------------
+
 function MyComponents() {
-  const [name, setName] = useState("guest");
-  const [age, setAge] = useState(0);
 
-  const[isEmployed,setisEmployed]=useState(false);
-  const updateName = () => {
-    setName("sponge");
-  };
+  const [name,setName]=useState("");
 
-  const updateAge = () => {
-    setAge(age + 1);
-  };
-
-  const toggleStatus=()=>{
-    setisEmployed(!isEmployed)
+  function handleNameChange(event) {
+    setName(event.target.value)
   }
 
-  return (
-    <div>
-      <p>Name:{name}</p>
-      <button onClick={updateName}>set Name</button>
-
-      <p>age:{age}</p>
-      <button onClick={updateAge}>set Name</button>
-
-      <p>isEmployed:{isEmployed?"yes":"no"}</p>
-      <button onClick={toggleStatus}>toogle status</button>
-    </div>
-  );
+  return (<>
+  <div>
+    <input value={name} onChange={handleNameChange} type="text" />
+    <p>name: {name} </p>
+  </div>
+  </>);
 }
-
 export default MyComponents;
